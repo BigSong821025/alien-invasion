7bf5f7cc85384d3f78afd6c9b7b5d68bce37b538
BigSong821025 <89571905+BigSong821025@users.noreply.github.com>
2021-08-26 16:28:58 +0800

    import Setting module, use its attributes
    import Setting module, use its attributes to rebuild run_game()

--- project_alien_invasion/alien_invasion.py
+++ project_alien_invasion/alien_invasion.py
@@ -7,10 +7,8 @@ from settings import Settings
 def run_game():
 	pygame.init()
 	ai_settings = Settings()
-	screen = pygame.display.set_mode((ai_settings.screen_width,800))
+	screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
-
-	bg_color = (230, 230, 230)
 
 	while True:
 
@@ -18,7 +16,7 @@ def run_game():
 			if event.type == pygame.QUIT:
 				sys.exit()
 
-		screen.fill(bg_color)
+		screen.fill(ai_settings.bg_color)
 
 		pygame.display.flip()
 
